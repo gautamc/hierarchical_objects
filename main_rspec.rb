2a68d955fe29371ddc3955ee23f3a2e227509119
mr. blue <gautam.chekuri@gmail.com>
2008-12-25 06:46:10 +0530

    made sortkey independent of id toensure object's order in tree is independent from its identity in tree

--- main_rspec.rb
+++ main_rspec.rb
@@ -21,11 +21,12 @@ describe HierarchicalObject, "when building a tree of objects" do
   before(:each) do
   end
   
-  it "should have empty sortkey and parent_id for root object" do
+  it "should have sortkey of /00 for root object" do
     root = HierarchicalObject.new
-    root.id = 1
+    root.id = 1999
     saved_p = root.save
-    root.sortkey.should be_empty
+    root.sortkey.should eql( "/00" )
+    root.parent_id.should be_nil
   end
   
   it "should not save subsequent root objects" do
@@ -37,15 +38,15 @@ describe HierarchicalObject, "when building a tree of objects" do
   
   it "should have a parent_id and sortkey for a non root object" do
     node2 = HierarchicalObject.new
-    node2.id = 2
-    node2.parent_id = 1
+    node2.id = 2876
+    node2.parent_id = 1999
     node2.save
-    node2.sortkey.should eql( "/02" )
+    node2.sortkey.should_not be_empty
   end
   
   it "should not allow parent_id that is not an id of an existing object" do
     node3 = HierarchicalObject.new
-    node3.id = 3
+    node3.id = 3543
     node3.parent_id = 420
     saved_p = node3.save
     saved_p.should be_false
@@ -53,41 +54,85 @@ describe HierarchicalObject, "when building a tree of objects" do
   
   it "should not allow of moving of an object into its own subtree" do
     node4 = HierarchicalObject.new
-    node4.id = 4
-    node4.parent_id = 2
+    node4.id = 4396
+    node4.parent_id = 2876
     node4.save
-    node4.sortkey.should eql( "/02/04" )
-
-    node2 = HierarchicalObject.find( 2 )
-    node2.parent_id = 4
+    node4.sortkey.should eql( "/00/00/00" )
+    
+    node2 = HierarchicalObject.find( 2876 )
+    node2.parent_id = 4396
     saved_p = node2.save
     
     saved_p.should be_false
-    node2.sortkey.should eql( "/02" )
+    node2.sortkey.should eql( "/00/00" )
   end
   
   it "should allow of moving of an object" do
     node3 = HierarchicalObject.new
-    node3.id = 3
-    node3.parent_id = 1
+    node3.id = 3543
+    node3.parent_id = 1999
     node3.save
     
-    node4 = HierarchicalObject.find( 4 )
-    node4.parent_id = 3
+    node5 = HierarchicalObject.new
+    node5.id = 5296
+    node5.parent_id = 1999
+    node5.save
+    
+    node4 = HierarchicalObject.find( 4396 )
+    node4.parent_id = 1999
     saved_p = node4.save
     
     saved_p.should be_true
-    node4.sortkey.should eql( "/03/04" )
-  end
-  
-  it "should update the all descendent objects when delete an ancestor object" do
-    node4 = HierarchicalObject.find( 4 )
-    node4.sortkey.should eql( "/03/04" )
-    HierarchicalObject.destroy( 3 )
+    node4.sortkey.should eql( "/00/03" )
+    
     node4.reload
-    node4.sortkey.should eql( "/04" )
+    node4.parent_id = 3543
+    saved_p = node4.save
+    
+    saved_p.should be_true
+    node4.sortkey.should eql( "/00/01/00" )
   end
   
+  it "should update child objects when of moving of a parent object" do
+    node6 = HierarchicalObject.new
+    node6.id = 6
+    node6.parent_id = 4396
+    node6.save
+    
+    node7 = HierarchicalObject.new
+    node7.id = 7
+    node7.parent_id = 4396
+    node7.save
+    
+    node8 = HierarchicalObject.new
+    node8.id = 8
+    node8.parent_id = 6
+    node8.save
+    
+    node4 = HierarchicalObject.find( 4396 )
+    node4.parent_id = 1999
+    saved_p = node4.save
+    
+    saved_p.should be_true
+    node4.sortkey.should eql( "/00/03" )
+    
+    node6.reload
+    node6.sortkey.should eql( "/00/03/00" )
+    
+    node7.reload
+    node7.sortkey.should eql( "/00/03/01" )
+    
+    node8.reload
+    node8.sortkey.should eql( "/00/03/00/00" )
+  end
+  
+  it "should not allow deletion of an object with descendant objects" do
+    node4 = HierarchicalObject.find( 4396 )
+    node4.sortkey.should eql( "/00/03" )
+    destroyed_p = HierarchicalObject.destroy( 4396 )
+    destroyed_p.should be_false
+  end
+
   it "should have depth" do
     treemaker_helper
     HierarchicalObject.find( 1 ).absdepth.should eql( 0 )
@@ -104,6 +149,26 @@ describe HierarchicalObject, "when building a tree of objects" do
     HierarchicalObject.find( 21 ).ancestors.length.should eql( 11 )
   end
   
+  it "should have sibilings" do
+    HierarchicalObject.find( 3 ).siblings.length.should eql( 2 )
+  end
+
+  it "should convert decimal to base159" do
+    HierarchicalObject.toBase159( 420 ).should eql( "2x" )
+    HierarchicalObject.toBase159( 421 ).should eql( "2X" )
+    HierarchicalObject.toBase159( 422 ).should eql( "2Y" )
+  end
+  
+  it "should increment a mvgid coded base159 number" do
+    HierarchicalObject.mvgid_base159_incr( "12x" ).should eql( HierarchicalObject.toBase159( 421 ) )
+    HierarchicalObject.mvgid_base159_incr( "12X" ).should eql( HierarchicalObject.toBase159( 422 ) )
+    HierarchicalObject.mvgid_base159_incr( "12Y" ).should eql( HierarchicalObject.toBase159( 423 ) )
+    
+    HierarchicalObject.mvgid_base159_incr(
+      HierarchicalObject.tomvgID( HierarchicalObject.toBase159( 400159 ) )
+    ).should eql( HierarchicalObject.toBase159( 400160 ) )
+  end
+  
   after(:each) do
   end
   
@@ -112,9 +177,14 @@ describe HierarchicalObject, "when building a tree of objects" do
   
   def treemaker_helper
     # delete the older objects
-    HierarchicalObject.destroy( 2 )
-    HierarchicalObject.destroy( 4 )
+    HierarchicalObject.find( 1999 ).subtree.reverse.each {
+      |obj|
+      obj.destroy
+    }
     # add the tree
+    root = HierarchicalObject.new
+    root.id = 1
+    saved_p = root.save
     nid = 2
     for i in 1 .. 1
       node = HierarchicalObject.new
--- models.rb
+++ models.rb
@@ -5,10 +5,10 @@ class HierarchicalObject < ActiveRecord::Base
   before_save lambda {
     |record|
     
-    # if root object(empty sortkey) doesn't already exist
+    # if root object(sortkey "/00") doesn't already exist
     if record.parent_id.nil?
-      unless exists?( :sortkey => "" )
-        record.sortkey = ""
+      unless exists?( :sortkey => "/00" )
+        record.sortkey = "/00"
         return true
       else
         return false
@@ -17,9 +17,16 @@ class HierarchicalObject < ActiveRecord::Base
     
     begin
       parent_rec = find( record.parent_id )
+      yngst_sibling_sortkey = maximum( :sortkey, :conditions => ['parent_id = ?', record.parent_id] )
       prev_sortkey = record.sortkey
-      base159_id = tomvgID( toBase159( record.id ) )
-      record.sortkey = parent_rec.sortkey + "/" + base159_id.to_s
+      
+      if ( yngst_sibling_sortkey.nil? )
+        skey = toBase159( 0 )
+      else
+        skey = mvgid_base159_incr( yngst_sibling_sortkey.match( /[^\/]+$/ ).to_s )
+      end
+      record.sortkey = parent_rec.sortkey + "/" + tomvgID( skey )
+      
       unless ( prev_sortkey.nil? )
         # currently, we don't allow moving a object in such a way that it becomes
         # a child object within the subtree rooted under it
@@ -28,11 +35,10 @@ class HierarchicalObject < ActiveRecord::Base
           return false
         end
         
-        # find the subtree in record, not including record
+        # update the subtree under record, not including record, based on recalculate sortkey of record
         transaction do
           if ( count(:conditions => ["sortkey like ?", prev_sortkey+"/%"]) > 0 )
-            #update the sortkeys in the subtree
-            update_all("sortkey=replace(sortkey, '" + prev_sortkey.gsub( /\/[^\/]+$/, '/') + "'," + "'" + parent_rec.sortkey+"/" + "')", ["sortkey like ?", prev_sortkey+"/%"])
+            update_all("sortkey=replace(sortkey, '" + prev_sortkey + "'," + "'" + record.sortkey + "')", ["sortkey like ?", prev_sortkey+"/%"])
           end
         end
       end
@@ -42,23 +48,22 @@ class HierarchicalObject < ActiveRecord::Base
     end
   }
   
-  # before deleteing an object, update sortkeys of all its child objects
+  # disallow object deletion if it has child objects
   before_destroy lambda {
     |record|
-    transaction do
-      update_all("sortkey=replace(sortkey, '" + record.sortkey+"/" + "'," + "'" + record.sortkey.gsub( /\/[^\/]+$/, '/') + "')", ["sortkey like ?", record.sortkey+"/%"])
-      update_all("parent_id = #{record.parent_id}", ["parent_id = ?", record.id])
+    if ( count(:conditions => ["parent_id = ?", record.id] ) > 0 )
+      return false
     end
     return true
   }
   
   def self.root
-    find( :first, :conditions => { :sortkey => "" } )
+    find( :first, :conditions => { :sortkey => "/00" } )
   end
   
   # depth with respect to root object
   def absdepth
-    sortkey.count("/") unless sortkey.nil?
+    sortkey.count("/")-1 unless sortkey.nil?
   end
   
   def subtree
@@ -72,14 +77,25 @@ class HierarchicalObject < ActiveRecord::Base
   def ancestors
     self.class.base_class.find(
       :all,
-      :conditions => ["locate(sortkey, ?) != 0", sortkey],
+      :conditions => ["locate(sortkey, ?) = 1", sortkey],
       :order => "sortkey"
     )
   end
   
-  protected
+  def siblings
+    self.class.base_class.find(
+      :all,
+      :conditions => [
+        "sortkey like ? and sortkey not like ?",
+        sortkey.gsub( /[^\/]+$/, '%' ),
+        sortkey.gsub( /[^\/]+$/, '%/%' )
+      ],
+      :order => "sortkey"
+    )
+  end
   
   @@enc_map = {}
+  @@enc_revmap = {}
   
   def self.toBase159(base10)
     if ( @@enc_map.keys.length == 0 )
@@ -96,6 +112,39 @@ class HierarchicalObject < ActiveRecord::Base
       break if( base10 == 0 )
     end
     base159
+  end
+  
+  def self.mvgid_base159_incr(base159)
+    if ( @@enc_revmap.keys.length == 0 )
+      @@enc_revmap = TreeEncoding.find( :all ).inject({}) {
+        |kvp, obj| kvp.merge! obj.code => obj.deci
+      }
+    end
+    
+    base159 ||= "0"
+    b159_num = base159.split( // )
+    result = []
+    carry = 1
+    # starting at with unit's position
+    (b159_num.length-1).downto(1) {
+      |pos|
+      # get decimal value for base159 digit at this position
+      deci = @@enc_revmap[b159_num[pos]]
+      # add carry (or 1 if unit's digit of b159_num) to decimal value, convert to base159
+      accum = toBase159(deci + carry).split( // )
+      # get unit's digit in accum and assign at current positional place of result
+      result.unshift( accum.last ) 
+      # assign the decimal value of the next digit of accum as carry
+      if( accum.length > 1 )
+        carry = @@enc_revmap[accum[0]]
+      else
+        carry = 0
+      end
+    }
+    if ( carry != 0 )
+      result.unshift( carry ) 
+    end
+    result.to_s
   end
   
   def self.tomvgID(base159)
